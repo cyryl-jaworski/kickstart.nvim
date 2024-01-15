ee5488e481f47640a183e3faf6b20c07684983f8
Cyryl Jaworski <cjaworski@Cyryls-MacBook-Pro.local>
2024-01-15 12:45:41 +0100

    theme changed to cat

--- init.lua
+++ init.lua
@@ -189,12 +189,22 @@ require('lazy').setup({
     },
   },
 
+  -- {
+  --   -- Theme inspired by Atom
+  --   'navarasu/onedark.nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'onedark'
+  --   end,
+  -- },
+
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    -- best theme
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+       vim.cmd.colorscheme "catppuccin-macchiato"
     end,
   },
 
